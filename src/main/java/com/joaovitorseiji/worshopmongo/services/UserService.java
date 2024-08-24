f7e298b034aaba637c9a249334e200754387fda1
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-08-24 13:44:55 -0300

    Obtendo um Usuario por Id - Boas Praticas com GET

--- src/main/java/com/joaovitorseiji/worshopmongo/services/UserService.java
+++ src/main/java/com/joaovitorseiji/worshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.joaovitorseiji.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaovitorseiji.worshopmongo.domain.User;
 import com.joaovitorseiji.worshopmongo.repository.UserRepository;
+import com.joaovitorseiji.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,17 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
-}
+	
+	public User findById(String id){
+		Optional <User> obj = userRepository.findById(id);
+		
+		if(obj.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado"); 
+		}else{
+			return obj.get();
+		}
+		}
+	}
+				
+		
+	
