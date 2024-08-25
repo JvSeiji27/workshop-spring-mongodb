23547625d0b83ec922fdc65d0040171ee3d40878
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-08-25 12:40:25 -0300

    Referenciando os posts do Usuario

--- src/main/java/com/joaovitorseiji/worshopmongo/config/Instantiation.java
+++ src/main/java/com/joaovitorseiji/worshopmongo/config/Instantiation.java
@@ -43,6 +43,10 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("24/08/2024"), "Bom Dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 	
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 	
 	
