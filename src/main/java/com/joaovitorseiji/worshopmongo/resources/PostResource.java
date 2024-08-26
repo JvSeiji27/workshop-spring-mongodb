2c6575f4249c91388fa8e87ca31a2db22d6dfdc7
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-08-26 17:15:46 -0300

    Consulta simples com query methods

--- src/main/java/com/joaovitorseiji/worshopmongo/resources/PostResource.java
+++ src/main/java/com/joaovitorseiji/worshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.joaovitorseiji.worshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaovitorseiji.worshopmongo.domain.Post;
+import com.joaovitorseiji.worshopmongo.resources.util.URL;
 import com.joaovitorseiji.worshopmongo.services.PostService;
 
 @RestController
@@ -22,5 +26,12 @@ public class PostResource {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
 		
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	ResponseEntity<List<Post>> findByTitle(@RequestParam (value ="text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List <Post> posts = postService.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
 	}
 }
--- src/main/java/com/joaovitorseiji/worshopmongo/services/PostService.java
+++ src/main/java/com/joaovitorseiji/worshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.joaovitorseiji.worshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.joaovitorseiji.worshopmongo.domain.Post;
 import com.joaovitorseiji.worshopmongo.repository.PostRepository;
+import com.joaovitorseiji.worshopmongo.repository.UserRepository;
 import com.joaovitorseiji.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -23,5 +25,9 @@ public class PostService{
 		}else {
 		return obj.get();		
 				}
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
